26024ad8f4551036d457484a1fcd50f8d45e0fff
yung-khajiit <jeenriquez1219@gmail.com>
2024-01-16 20:46:08 -0600

    Modified App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Jesse's Advance Web Design" />
 </template>
 
 <style scoped>
